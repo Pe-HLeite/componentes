3f1189dbae2715151d89153b786b7a3fc4740a24
= <pedrohleite28@gmail.com>
2025-08-21 20:39:16 -0300

    Configurando a extenção do prettier e utilizando o formatador

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,15 @@
-<script>
-</script>
+<script></script>
 
 <template>
-    <h1 class="text-3xl font-bold text-red-600">
-        Utilizando classe tailwind
-    </h1>
+    <div class="w-full items-center bg-gray-200 p-2 pb-[14px] text-center">
+        <h1 class="text-4xl font-semibold">Testando Componente Rich Text</h1>
+    </div>
+
+    <div class="m-3 flex justify-center bg-orange-400 p-5">
+        <p>Esse é o meu contéudo</p>
+    </div>
+
+    <!-- <div class="m-3 flex w-full items-center justify-end bg-zinc-400 p-5">
+        <p>Esse é o meu contéudo</p>
+    </div> -->
 </template>
